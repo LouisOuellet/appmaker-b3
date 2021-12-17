6eaec4ea9dac3c06cd538a24b24d80be5ba99d64
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-17 13:13:12 -0500

    21.12-dev-7

--- api.php
+++ api.php
@@ -30,13 +30,29 @@ class b3API extends CRUDAPI {
 					}
 				}
 				$b3 = $this->Auth->query('SELECT * FROM `b3` WHERE `transaction_number` = ?',$metaData['transaction_number'])->fetchAll()->all();
-				if(!empty($b3)){
-					$b3 = $b3[0];
-				} else {
-					$b3ID = $this->Auth->create('b3',$b3);
-					$b3 = $this->Auth->read('b3',$b3ID)->all()[0];
+				$organization = $this->Auth->query('SELECT * FROM `organizations` WHERE `setCodeHVS` LIKE ? OR `setCodeLVS` LIKE ?',(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)),(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)))->fetchAll()->all();
+				if(!empty($organization)){
+					$organization = $organization[0];
+					if(!empty($b3)){
+						$b3 = $b3[0];
+						$this->createRelationship([
+							'relationship_1' => 'b3',
+							'link_to_1' => $b3['id'],
+							'relationship_2' => 'organizations',
+							'link_to_2' => $organization['id'],
+						]);
+					} else {
+						$b3ID = $this->Auth->create('b3',$b3);
+						$b3 = $this->Auth->read('b3',$b3ID)->all()[0];
+						$this->createRelationship([
+							'relationship_1' => 'b3',
+							'link_to_1' => $b3['id'],
+							'relationship_2' => 'organizations',
+							'link_to_2' => $organization['id'],
+						]);
+					}
+					$this->copyRelationships($type,$record['id'],'b3',$b3['id']);
 				}
-				$this->copyRelationships($type,$record['id'],'b3',$b3['id']);
 				break;
 			default: break;
 		}
