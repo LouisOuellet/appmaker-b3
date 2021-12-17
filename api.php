4761f2831082022aa7e54f08aa8820e47377101d
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-17 15:28:58 -0500

    21.12-dev-16

--- api.php
+++ api.php
@@ -42,29 +42,32 @@ class b3API extends CRUDAPI {
 						default: break;
 					}
 				}
-				$b3 = $this->Auth->query('SELECT * FROM `b3` WHERE `transaction_number` = ?',$metaData['transaction_number'])->fetchAll()->all();
-				$organization = $this->Auth->query('SELECT * FROM `organizations` WHERE `setCodeHVS` LIKE ? OR `setCodeLVS` LIKE ?',(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)),(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)))->fetchAll()->all();
-				if(!empty($organization)){
-					$organization = $organization[0];
-					if(!empty($b3)){
-						$b3 = $b3[0];
-						$this->createRelationship([
-							'relationship_1' => 'b3',
-							'link_to_1' => $b3['id'],
-							'relationship_2' => 'organizations',
-							'link_to_2' => $organization['id'],
-						]);
-					} else {
-						$b3ID = $this->Auth->create('b3',$metaData);
-						$b3 = $this->Auth->read('b3',$b3ID)->all()[0];
-						$this->createRelationship([
-							'relationship_1' => 'b3',
-							'link_to_1' => $b3['id'],
-							'relationship_2' => 'organizations',
-							'link_to_2' => $organization['id'],
-						]);
+				if(isset($metaData['transaction_number'])){
+					$b3 = $this->Auth->query('SELECT * FROM `b3` WHERE `transaction_number` = ?',$metaData['transaction_number'])->fetchAll()->all();
+					$organization = $this->Auth->query('SELECT * FROM `organizations` WHERE `setCodeHVS` LIKE ? OR `setCodeLVS` LIKE ?',(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)),(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)))->fetchAll()->all();
+					if(!empty($organization)){
+						$organization = $organization[0];
+						if(!empty($b3)){
+							$b3 = $b3[0];
+							$this->createRelationship([
+								'relationship_1' => 'b3',
+								'link_to_1' => $b3['id'],
+								'relationship_2' => 'organizations',
+								'link_to_2' => $organization['id'],
+							]);
+						} else {
+							$b3ID = $this->Auth->create('b3',$metaData);
+							$b3 = $this->Auth->read('b3',$b3ID)->all()[0];
+							$this->createRelationship([
+								'relationship_1' => 'b3',
+								'link_to_1' => $b3['id'],
+								'relationship_2' => 'organizations',
+								'link_to_2' => $organization['id'],
+							]);
+						}
+						$this->copyRelationships($type,$record['id'],'b3',$b3['id']);
 					}
-					$this->copyRelationships($type,$record['id'],'b3',$b3['id']);
+					if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$metaData['transaction_number']."]Updating B3\n"; }
 				}
 				break;
 			default: break;
