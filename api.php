27dea3806cb332d950d2d00d6e4a4bb43fb1bd3d
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-17 19:39:54 -0500

    21.12-dev-28

--- api.php
+++ api.php
@@ -40,45 +40,45 @@ class b3API extends CRUDAPI {
 						default: break;
 					}
 				}
-				$b3 = $this->Auth->query('SELECT * FROM `b3` WHERE `transaction_number` = ?',$metaData['transaction_number'])->fetchAll()->all();
-				$organization = $this->Auth->query('SELECT * FROM `organizations` WHERE `setCodeHVS` LIKE ? OR `setCodeLVS` LIKE ?',(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)),(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)))->fetchAll()->all();
-				if(!empty($organization)){
-					$organization = $organization[0];
-					if(!empty($b3)){
-						$b3 = $b3[0];
-						$action = "Updating";
-					} else {
-						$b3ID = $this->Auth->create('b3',$metaData);
-						$b3 = $this->Auth->read('b3',$b3ID)->all()[0];
-						$action = "Creating";
-					}
-					// Load Relationships
-					$relationships = $this->getRelationships('b3',$b3['id']);
-					$lastID = 0;
-					foreach($relationships as $id => $relationship){
-						if($lastID < $id){ $lastID = $id; }
-					}
-					$this->createRelationship([
-						'relationship_1' => 'b3',
-						'link_to_1' => $b3['id'],
-						'relationship_2' => 'organizations',
-						'link_to_2' => $organization['id'],
-					]);
-					$this->copyRelationships($type,$record['id'],'b3',$b3['id']);
-					// Reload Relationships
-					$relationships = $this->getRelationships('b3',$b3['id']);
-					$newID = 0;
-					foreach($relationships as $id => $relationship){
-						if($newID < $id){ $newID = $id; }
-					}
-					if($lastID < $newID){
-						if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$newID."]".$action." B3: ".$metaData['transaction_number']."\n"; }
-					}
-				} else {
-					if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "Unable to find organization with [setCodeHVS] or [setCodeLVS] of: ".(substr(str_replace('-','',$metaData['transaction_number']), 0, 5))."\n"; }
-				}
 				break;
 			default: break;
+		}
+		$b3 = $this->Auth->query('SELECT * FROM `b3` WHERE `transaction_number` = ?',$metaData['transaction_number'])->fetchAll()->all();
+		$organization = $this->Auth->query('SELECT * FROM `organizations` WHERE `setCodeHVS` LIKE ? OR `setCodeLVS` LIKE ?',(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)),(substr(str_replace('-','',$metaData['transaction_number']), 0, 5)))->fetchAll()->all();
+		if(!empty($organization)){
+			$organization = $organization[0];
+			if(!empty($b3)){
+				$b3 = $b3[0];
+				$action = "Updating";
+			} else {
+				$b3ID = $this->Auth->create('b3',$metaData);
+				$b3 = $this->Auth->read('b3',$b3ID)->all()[0];
+				$action = "Creating";
+			}
+			// Load Relationships
+			$relationships = $this->getRelationships('b3',$b3['id']);
+			$lastID = 0;
+			foreach($relationships as $id => $relationship){
+				if($lastID < $id){ $lastID = $id; }
+			}
+			$this->createRelationship([
+				'relationship_1' => 'b3',
+				'link_to_1' => $b3['id'],
+				'relationship_2' => 'organizations',
+				'link_to_2' => $organization['id'],
+			]);
+			$this->copyRelationships($type,$record['id'],'b3',$b3['id']);
+			// Reload Relationships
+			$relationships = $this->getRelationships('b3',$b3['id']);
+			$newID = 0;
+			foreach($relationships as $id => $relationship){
+				if($newID < $id){ $newID = $id; }
+			}
+			if($lastID < $newID){
+				if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$newID."]".$action." B3: ".$metaData['transaction_number']."\n"; }
+			}
+		} else {
+			if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "Unable to find organization with [setCodeHVS] or [setCodeLVS] of: ".(substr(str_replace('-','',$metaData['transaction_number']), 0, 5))."\n"; }
 		}
 	}
 }
