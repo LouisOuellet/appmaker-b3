9126c606a534ec716b5ab261c640bdd77a92c66c
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 15:47:15 -0500

    22.01-dev-103

--- api.php
+++ api.php
@@ -7,6 +7,7 @@ class b3API extends CRUDAPI {
 			$this->Auth->setLimit(0);
 			// Load Event
 			$get = parent::get('b3', $data);
+			var_dump($get);
 			// Build Relations
 			$get = $this->buildRelations($get);
 			var_dump($get);
