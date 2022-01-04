e887d309c90ecb08d66d9908f9d6608eda51cc91
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 09:51:28 -0500

    22.01-dev-81

--- api.php
+++ api.php
@@ -19,9 +19,9 @@ class b3API extends CRUDAPI {
 		if(isset($this->Settings['plugins']['conversations']['status']) && $this->Settings['plugins']['conversations']['status']){
 			$scan['conversations'] = $this->Auth->query('SELECT * FROM `conversations` WHERE `meta` LIKE ? AND `hasNew` = ?','%TR:%','true')->fetchAll()->all();
 		}
-		if(isset($this->Settings['plugins']['messages']['status']) && $this->Settings['plugins']['messages']['status']){
-			$scan['messages'] = $this->Auth->query('SELECT * FROM `messages` WHERE `meta` NOT LIKE ?','%scanB3%')->fetchAll()->all();
-		}
+		// if(isset($this->Settings['plugins']['messages']['status']) && $this->Settings['plugins']['messages']['status']){
+		// 	$scan['messages'] = $this->Auth->query('SELECT * FROM `messages` WHERE `meta` NOT LIKE ?','%scanB3%')->fetchAll()->all();
+		// }
 		foreach($scan as $table => $records){
 			if(!empty($records)){
 				foreach($records as $record){
