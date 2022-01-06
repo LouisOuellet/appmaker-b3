41c8bda9310c085224daefe03d2a314f602e01f5
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-05 19:00:59 -0500

    22.01-dev-115

--- api.php
+++ api.php
@@ -145,6 +145,47 @@ class b3API extends CRUDAPI {
 							$this->copyRelationships($table,$record['id'],'b3',$b3['id']);
 							// Reload Relationships
 							$relationships = $this->getRelationships('b3',$b3['id']);
+							// Updating B3
+							foreach($relationships as $id => $relationship){
+								foreach($relationship as $relation){
+									if($relation['relationship'] == 'messages'){
+										// run through messages here instead
+										$message = $this->Auth->query('SELECT * FROM `messages` WHERE `id` = ?',$relation['link_to'])->fetchAll()->all();
+										if(!empty($message)){
+											$message = $message[0];
+											if($message['meta'] != '' && $message['meta'] != null && !is_array($message['meta'])){
+												$message['meta'] = json_decode($message['meta'], true);
+											} else { $message['meta'] = []; }
+											if(!in_array('scanB3',$message['meta'])){
+												$current = $b3['status'];
+												if(strpos($message['to'], 'create@') !== false && $current < 4){$b3['status'] = 3;}
+												if(strpos($message['to'], 'reject@') !== false && $current < 4){$b3['status'] = 4;}
+												if(strpos($message['to'], 'release@') !== false && $current < 6){$b3['status'] = 6;}
+												if(strpos($message['to'], 'billed@') !== false && $current < 9){$b3['status'] = 9;}
+												if(strpos($message['to'], 'done@') !== false && $current < 11){$b3['status'] = 11;}
+												if(strpos($message['to'], 'cancel@') !== false && $current < 12){$b3['status'] = 12;}
+												if(isset($this->Settings['debug']) && $this->Settings['debug'] && $current != $b3['status']){ echo "[".$message['to']."]"."[".$b3['transaction_number']."] changing status from: ".$current." to: ".$b3['status']."\n"; }
+												if($current != $b3['status']){
+													$status = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?','b3',$b3['status'])->fetchAll()->all();
+													if(!empty($status)){
+														$this->createRelationship([
+															'relationship_1' => 'b3',
+															'link_to_1' => $b3['id'],
+															'relationship_2' => 'statuses',
+															'link_to_2' => $status[0]['id'],
+														],true);
+													}
+												}
+												array_push($message['meta'],'scanB3');
+												$message['meta'] = json_encode($message['meta'], JSON_PRETTY_PRINT);
+												$this->Auth->update('messages',$message,$message['id']);
+											}
+										}
+									}
+								}
+							}
+							// Check if B3 was updated
+							$relationships = $this->getRelationships('b3',$b3['id']);
 							$newID = 0;
 							foreach($relationships as $id => $relationship){
 								if($newID < $id){ $newID = $id; }
