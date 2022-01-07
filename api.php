89ec0e965a2a7acaf0b72b3de5b1277b8f4a9f6f
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-07 09:40:11 -0500

    22.01-dev-126

--- api.php
+++ api.php
@@ -28,7 +28,7 @@ class b3API extends CRUDAPI {
 	public function scan(){
 		$scan = [];
 		if(isset($this->Settings['plugins']['conversations']['status']) && $this->Settings['plugins']['conversations']['status']){
-			$scan['conversations'] = $this->Auth->query('SELECT * FROM `conversations` WHERE `meta` LIKE ? AND `hasNew` = ?','%TR:%','true')->fetchAll()->all();
+			$scan['conversations'] = $this->Auth->query('SELECT * FROM `conversations` WHERE `meta` LIKE ? AND `hasNew` = ? AND `status` = ?','%TR:%','true',1)->fetchAll()->all();
 		}
 		foreach($scan as $table => $records){
 			if(!empty($records)){
@@ -82,105 +82,106 @@ class b3API extends CRUDAPI {
 								}
 								$action = "Creating";
 							}
-							$current = $b3['status'];
-							// Load Relationships
-							$relationships = $this->getRelationships('b3',$b3['id']);
-							$lastID = 0;
-							foreach($relationships as $id => $relationship){ if($lastID < $id){ $lastID = $id; } }
-							// Link the Organization
-							$this->createRelationship([
-								'relationship_1' => 'b3',
-								'link_to_1' => $b3['id'],
-								'relationship_2' => 'organizations',
-								'link_to_2' => $organization['id'],
-							]);
-							// Link the record
-							$this->createRelationship([
-								'relationship_1' => 'b3',
-								'link_to_1' => $b3['id'],
-								'relationship_2' => $table,
-								'link_to_2' => $record['id'],
-							]);
-							// Copy Relationship from record
-							$this->copyRelationships($table,$record['id'],'b3',$b3['id']);
-							// Reload Relationships
-							$relationships = $this->getRelationships('b3',$b3['id']);
-							// Updating B3
-							foreach($relationships as $id => $relationship){
-								foreach($relationship as $relation){
-									if($relation['relationship'] == 'messages'){
-										// Updating B3 based on messages
-										$message = $this->Auth->query('SELECT * FROM `messages` WHERE `id` = ?',$relation['link_to'])->fetchAll()->all();
-										if(!empty($message)){
-											$message = $message[0];
-											if($message['meta'] != '' && $message['meta'] != null && !is_array($message['meta'])){
-												$message['meta'] = json_decode($message['meta'], true);
-											} else { $message['meta'] = []; }
-											if(!in_array('scanB3',$message['meta'])){
-												$current = $b3['status'];
-												if(strpos($message['to'], 'create@') !== false && $current < 4){$b3['status'] = 3;}
-												if(strpos($message['to'], 'reject@') !== false && $current < 4){$b3['status'] = 4;}
-												if(strpos($message['to'], 'release@') !== false && $current < 6){$b3['status'] = 6;}
-												if(strpos($message['to'], 'billed@') !== false && $current < 9){$b3['status'] = 9;}
-												if(strpos($message['to'], 'done@') !== false && $current < 11){$b3['status'] = 11;}
-												if(strpos($message['to'], 'cancel@') !== false && $current < 12){$b3['status'] = 12;}
-												if(isset($this->Settings['debug']) && $this->Settings['debug'] && $current != $b3['status']){ echo "[".$message['to']."]"."[".$b3['transaction_number']."] changing status from: ".$current." to: ".$b3['status']."\n"; }
-												if($current != $b3['status']){
-													$status = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?','b3',$b3['status'])->fetchAll()->all();
+							if($b3['status'] < 11){
+								$current = $b3['status'];
+								// Load Relationships
+								$relationships = $this->getRelationships('b3',$b3['id']);
+								$lastID = 0;
+								foreach($relationships as $id => $relationship){ if($lastID < $id){ $lastID = $id; } }
+								// Link the Organization
+								$this->createRelationship([
+									'relationship_1' => 'b3',
+									'link_to_1' => $b3['id'],
+									'relationship_2' => 'organizations',
+									'link_to_2' => $organization['id'],
+								]);
+								// Link the record
+								$this->createRelationship([
+									'relationship_1' => 'b3',
+									'link_to_1' => $b3['id'],
+									'relationship_2' => $table,
+									'link_to_2' => $record['id'],
+								]);
+								// Copy Relationship from record
+								$this->copyRelationships($table,$record['id'],'b3',$b3['id']);
+								// Reload Relationships
+								$relationships = $this->getRelationships('b3',$b3['id']);
+								// Updating B3
+								foreach($relationships as $id => $relationship){
+									foreach($relationship as $relation){
+										if($relation['relationship'] == 'messages'){
+											// Updating B3 based on messages
+											$message = $this->Auth->query('SELECT * FROM `messages` WHERE `id` = ?',$relation['link_to'])->fetchAll()->all();
+											if(!empty($message)){
+												$message = $message[0];
+												if($message['meta'] != '' && $message['meta'] != null && !is_array($message['meta'])){
+													$message['meta'] = json_decode($message['meta'], true);
+												} else { $message['meta'] = []; }
+												if(!in_array('scanB3',$message['meta'])){
+													$current = $b3['status'];
+													if(strpos($message['to'], 'create@') !== false && $current < 4){$b3['status'] = 3;}
+													if(strpos($message['to'], 'reject@') !== false && $current < 4){$b3['status'] = 4;}
+													if(strpos($message['to'], 'release@') !== false && $current < 6){$b3['status'] = 6;}
+													if(strpos($message['to'], 'billed@') !== false && $current < 9){$b3['status'] = 9;}
+													if(strpos($message['to'], 'done@') !== false && $current < 11){$b3['status'] = 11;}
+													if(strpos($message['to'], 'cancel@') !== false && $current < 12){$b3['status'] = 12;}
+													if(isset($this->Settings['debug']) && $this->Settings['debug'] && $current != $b3['status']){ echo "[".$message['to']."]"."[".$b3['transaction_number']."] changing status from: ".$current." to: ".$b3['status']."\n"; }
+													if($current != $b3['status']){
+														$status = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?','b3',$b3['status'])->fetchAll()->all();
+														if(!empty($status)){
+															$this->createRelationship([
+																'relationship_1' => 'b3',
+																'link_to_1' => $b3['id'],
+																'relationship_2' => 'statuses',
+																'link_to_2' => $status[0]['id'],
+															],true);
+														}
+													}
+													array_push($message['meta'],'scanB3');
+													$message['meta'] = json_encode($message['meta'], JSON_PRETTY_PRINT);
+													$this->Auth->update('messages',$message,$message['id']);
+													if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$message['id']."] Message tagged!\n"; }
+												}
+											}
+										}
+										// Closing open conversations
+										if($relation['relationship'] == 'conversations'){
+											if($b3['status'] >= 11){
+												$conversation = $this->Auth->query('SELECT * FROM `conversations` WHERE `id` = ? AND `status` = ?',$relation['link_to'],1)->fetchAll()->all();
+												if(!empty($conversation)){
+													$conversation = $conversation[0];
+													$conversation['status'] = 3;
+													$status = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?','conversations',$conversation['status'])->fetchAll()->all();
 													if(!empty($status)){
 														$this->createRelationship([
-															'relationship_1' => 'b3',
-															'link_to_1' => $b3['id'],
+															'relationship_1' => 'conversations',
+															'link_to_1' => $record['id'],
 															'relationship_2' => 'statuses',
 															'link_to_2' => $status[0]['id'],
-														],true);
+														]);
+														$this->Auth->update('conversations',$conversation,$conversation['id']);
+														if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$conversation['id']."] Conversation Closed!\n"; }
 													}
 												}
-												array_push($message['meta'],'scanB3');
-												$message['meta'] = json_encode($message['meta'], JSON_PRETTY_PRINT);
-												$this->Auth->update('messages',$message,$message['id']);
-												if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$message['id']."] Message tagged!\n"; }
-											}
-										}
-									}
-									// Closing open conversations
-									if($relation['relationship'] == 'conversations'){
-										if($b3['status'] >= 11){
-											$conversation = $this->Auth->query('SELECT * FROM `conversations` WHERE `id` = ?',$relation['link_to'])->fetchAll()->all();
-											if(!empty($conversation)){
-												$conversation = $conversation[0];
-												$conversation['status'] = 3;
-												$status = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?','conversations',$conversation['status'])->fetchAll()->all();
-												if(!empty($status)){
-													$this->createRelationship([
-														'relationship_1' => 'conversations',
-														'link_to_1' => $record['id'],
-														'relationship_2' => 'statuses',
-														'link_to_2' => $status[0]['id'],
-													],true);
-													$this->Auth->update('conversations',$conversation,$conversation['id']);
-													if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$conversation['id']."] Conversation Closed!\n"; }
-												}
 											}
 										}
 									}
 								}
-							}
-							// Check if B3 was updated
-							$relationships = $this->getRelationships('b3',$b3['id']);
-							$newID = 0;
-							foreach($relationships as $id => $relationship){ if($newID < $id){ $newID = $id; } }
-							// Display if B3 was updated
-							if($lastID < $newID){
-								if(isset($record['hasNew']) && in_array($table,['conversations'])){
-									$record['hasNew'] = '';
-									$this->Auth->update($table,$record,$record['id']);
-									if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$table."] Updating record: [".$record['id']."]\n"; }
+								// Check if B3 was updated
+								$relationships = $this->getRelationships('b3',$b3['id']);
+								$newID = 0;
+								foreach($relationships as $id => $relationship){ if($newID < $id){ $newID = $id; } }
+								// Display if B3 was updated
+								if($lastID < $newID){
+									if(isset($record['hasNew']) && in_array($table,['conversations'])){
+										$record['hasNew'] = '';
+										$this->Auth->update($table,$record,$record['id']);
+										if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$table."] Updating record: [".$record['id']."]\n"; }
+									}
+									$this->Auth->update('b3',$b3,$b3['id']);
+									if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$newID."]".$action." B3: ".$metaData['transaction_number']."\n"; }
 								}
-								$this->Auth->update('b3',$b3,$b3['id']);
-								if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$newID."]".$action." B3: ".$metaData['transaction_number']."\n"; }
 							}
-							//
 						} else {
 							if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "Unable to find organization with [setCodeHVS] or [setCodeLVS] of: ".(substr(str_replace('-','',$metaData['transaction_number']), 0, 5))."\n"; }
 						}
